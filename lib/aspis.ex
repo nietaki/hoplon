fb5d73d4cba60be09a87062f83cc1eece05460ce
Jacek Królikowski <nietaki@gmail.com>
2018-04-03 09:09:28 +0100

    Pull all the check_package code out of a mix task

--- lib/aspis.ex
+++ lib/aspis.ex
@@ -2,6 +2,8 @@ defmodule Aspis do
   alias Aspis.Diff
   alias Aspis.Git
   alias Aspis.Utils
+  alias Aspis.HexPackage
+  alias Aspis.CheckResult
 
   @program_dependencies ["git", "diff"]
 
@@ -32,6 +34,44 @@ defmodule Aspis do
 
       {:error, _} ->
         Git.attempt_checkout("v" <> version, cd_path)
+    end
+  end
+
+  def check_package(package = %HexPackage{}, git_parent_directory) do
+    {:ok, project_deps_path} = Utils.get_project_deps_path()
+    repo_path = Path.join(git_parent_directory, Atom.to_string(package.name))
+    dep_path = Path.join(project_deps_path, Atom.to_string(package.name))
+
+    with result = CheckResult.new(package),
+         {:ok, result} <- add_git_url(result),
+         {:ok, _} <- Aspis.prepare_repo(result.git_url, repo_path),
+         {:ok, result} <- checkout_version(result, repo_path),
+         diffs = Aspis.get_relevant_file_diffs(repo_path, dep_path),
+         result = %CheckResult{result | diffs: diffs} do
+      result
+    else
+      {:error, result = %CheckResult{}} ->
+        result
+    end
+  end
+
+  defp add_git_url(result) do
+    case Utils.get_github_git_url(result.hex_package.hex_name) do
+      {:ok, git_url} ->
+        {:ok, %CheckResult{result | git_url: git_url}}
+
+      {:error, reason} ->
+        {:error, CheckResult.set_error_reason(result, reason)}
+    end
+  end
+
+  defp checkout_version(result, repo_path) do
+    case Aspis.checkout_version_by_tag(result.hex_package.version, repo_path) do
+      {:ok, version_tag} ->
+        {:ok, %CheckResult{result | git_ref: {:tag, version_tag}}}
+
+      {:error, {:invalid_ref, _}} ->
+        {:error, CheckResult.set_error_reason(result, :could_not_find_git_tag)}
     end
   end
 
--- lib/mix/tasks/aspis.check.ex
+++ lib/mix/tasks/aspis.check.ex
@@ -11,7 +11,6 @@ defmodule Mix.Tasks.Aspis.Check do
 
   def run(_args) do
     with {:ok, _} <- Aspis.check_required_programs(),
-         {:ok, deps} <- Utils.get_project_deps(),
          {:ok, hex_packages_from_mix_lock} <- Utils.get_packages_from_mix_lock(),
          {:ok, project_deps_path} <- Utils.get_project_deps_path() do
       # TODO figure out if all of those are necessarily used or packages get kept in mix_lock after they stop being used
@@ -21,20 +20,7 @@ defmodule Mix.Tasks.Aspis.Check do
 
       results =
         Stream.map(relevant_packages, fn package ->
-          repo_path = Path.join(@git_parent_directory, Atom.to_string(package.name))
-          dep_path = Path.join(project_deps_path, Atom.to_string(package.name))
-
-          with result = CheckResult.new(package),
-               {:ok, result} <- add_git_url(result),
-               {:ok, _} <- Aspis.prepare_repo(result.git_url, repo_path),
-               {:ok, result} <- checkout_version(result, repo_path),
-               diffs = Aspis.get_relevant_file_diffs(repo_path, dep_path),
-               result = %CheckResult{result | diffs: diffs} do
-            result
-          else
-            {:error, result = %CheckResult{}} ->
-              result
-          end
+          Aspis.check_package(package, @git_parent_directory)
         end)
         |> Stream.each(fn r ->
           r |> CheckResult.representation_line() |> IO.puts()
@@ -51,26 +37,6 @@ defmodule Mix.Tasks.Aspis.Check do
     else
       {:error, reason} when is_atom(reason) ->
         task_exit(1, inspect(reason))
-    end
-  end
-
-  defp add_git_url(result) do
-    case Utils.get_github_git_url(result.hex_package.hex_name) do
-      {:ok, git_url} ->
-        {:ok, %CheckResult{result | git_url: git_url}}
-
-      {:error, reason} ->
-        {:error, CheckResult.set_error_reason(result, reason)}
-    end
-  end
-
-  defp checkout_version(result, repo_path) do
-    case Aspis.checkout_version_by_tag(result.hex_package.version, repo_path) do
-      {:ok, version_tag} ->
-        {:ok, %CheckResult{result | git_ref: {:tag, version_tag}}}
-
-      {:error, {:invalid_ref, _}} ->
-        {:error, CheckResult.set_error_reason(result, :could_not_find_git_tag)}
     end
   end
 
