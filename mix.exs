2a07516e0dc05175513894169ef9235af2e0fe91
Jacek Królikowski <nietaki@gmail.com>
2018-04-14 21:29:25 +0100

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Hoplon.MixProject do
   def project do
     [
       app: :hoplon,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.4",
       package: package(),
       start_permanent: false,
