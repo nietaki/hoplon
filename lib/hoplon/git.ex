691d896debb9239ccbf239ea97334c2251200e12
Jacek Królikowski <nietaki@gmail.com>
2018-04-16 00:00:53 +0100

    Fix remaining github url assumption

--- lib/hoplon/git.ex
+++ lib/hoplon/git.ex
@@ -142,7 +142,7 @@ defmodule Hoplon.Git do
 
   # NOTE: this doesn't really validate the git url
   def get_github_user_and_package_from_git_url(git_url) when is_binary(git_url) do
-    case Regex.run(~r{([\w_-]+)/([\w_-]+).git$}, git_url) do
+    case Regex.run(~r{([\w_.-]+)/([\w_.-]+).git$}, git_url) do
       [_whole, user, repo_name] ->
         {:ok, {user, repo_name}}
 
