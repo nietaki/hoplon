f7b1e496f97bcb82b8cdbadd39120c18a152b312
Jacek Królikowski <nietaki@gmail.com>
2019-02-28 17:44:30 +0000

    Add an integration test (#35)
    * Add an integration test
    
    * Run integration test(s) before formatting checks
    
    * Use a https url for suffixer repo

---
+++ scripts/integration_test.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# make sure we're in the correct directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd $DIR/..
+
+printf "\n# cloning suffixer\n\n"
+
+rm -rf suffixer
+
+git clone --branch hoplon-integration-test https://github.com/nietaki/suffixer.git || exit 1
+
+pushd suffixer
+
+printf "\n# getting dependencies and compiling\n\n"
+
+mix clean || exit 1
+mix deps.get || exit 1
+mix compile 2>/dev/null || exit 1
+
+printf "\n# checking if hoplon works \n\n"
+
+mix hoplon.check
+# this should return a "12" exit code
+rc=$?; if [[ $rc != 12 ]]; then exit 1; fi
+
+# a loose assertion on the error message
+mix hoplon.check | grep evil_left_pad | grep CORRUPT || exit 1
+
+printf "\n## INTEGRATION TESTS SUCCEEDED ##\n\n"
+
+popd
+popd
+exit 0
