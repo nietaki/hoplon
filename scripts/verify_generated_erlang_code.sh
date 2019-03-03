e0428c09efb0bbfb81b27538c25edf0420a65b89
Jacek Królikowski <nietaki@gmail.com>
2019-03-03 16:13:50 +0000

    See what the generated erlang changes are in practice

--- scripts/verify_generated_erlang_code.sh
+++ scripts/verify_generated_erlang_code.sh
@@ -4,9 +4,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR/..
 
+cp -R src/generated/ /tmp/hoplon_src_generated
+
 hashdeep -c sha256 -r src | sort > /tmp/hoplon_src_before.txt
 mix compile
 hashdeep -c sha256 -r src | sort > /tmp/hoplon_src_after.txt
+
+diff /tmp/hoplon_src_generated/ src/generated
 
 diff /tmp/hoplon_src_before.txt /tmp/hoplon_src_after.txt || exit 1
 
