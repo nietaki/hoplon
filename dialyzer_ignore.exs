9560f4f17237c37b49925bc3c8bacf49e168b38e
Jacek Królikowski <nietaki@gmail.com>
2019-04-09 22:57:04 +0100

    Exclude test support files from dialyzer

--- dialyzer_ignore.exs
+++ dialyzer_ignore.exs
@@ -1,8 +1,9 @@
 [
   ~r/Hex.API/,
   # temporarily ignoring the old problems so that we can keep the new code clean more easily
-  ~r{lib/mix/tasks/.*},
-  ~r{lib/hoplon/lockfile.ex},
+  ~r{^lib/mix/tasks/.*},
+  ~r{^lib/hoplon/lockfile.ex},
   ~r{asn1rt},
-  ~r{src/generated/HoplonMessages.erl}
+  ~r{^src/generated/HoplonMessages.erl},
+  ~r{^test/support/.*}
 ]
